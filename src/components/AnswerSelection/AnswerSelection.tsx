9bbf91543aa18194833f4779918680a42a825a49
benhjt <benhjt@users.noreply.github.com>
2023-08-23 23:55:36 +0100

    Removes AnswerButton component

--- src/components/AnswerSelection/AnswerSelection.tsx
+++ src/components/AnswerSelection/AnswerSelection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { NOTES } from '../../constants';
-import AnswerButton from '../AnswerButton';
 
 interface AnswerSelectionProps {
   onAnswerSelected: (value: string | undefined) => void;
@@ -12,12 +11,14 @@ function AnswerSelection({ onAnswerSelected }: AnswerSelectionProps) {
     <div className="grid grid-cols-6 gap-1 w-60">
       {NOTES.map((note) => (
         <div key={note}>
-          <AnswerButton
+          <button
+            type="button"
+            className="inline-flex items-center justify-center align-midddle min-w-[36px] px-2 py-1 text-neutral-200 bg-slate-500 hover:bg-slate-700 shadow-sm select-none cursor-pointer outline-none border-none rounded-md uppercase transition-colors"
+            onClick={() => onAnswerSelected(note)}
             value={note}
-            onClick={(value) => onAnswerSelected(value)}
           >
             {note}
-          </AnswerButton>
+          </button>
         </div>
       ))}
     </div>
